bb316e01fea1dc9b09558cfbcf8ba04c3f5d382d
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2016-04-23 21:15:30 +0200

    update customer controller

--- src/main/java/at/ac/tuwien/cashtechthon/controller/CustomerController.java
+++ src/main/java/at/ac/tuwien/cashtechthon/controller/CustomerController.java
@@ -2,6 +2,9 @@ package at.ac.tuwien.cashtechthon.controller;
 
 import javax.websocket.server.PathParam;
 
+import at.ac.tuwien.cashtechthon.dao.ICustomerDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,19 +20,26 @@ import at.ac.tuwien.cashtechthon.dtos.CustomerData;
 @RequestMapping("/customers")
 public class CustomerController extends AbstractController {
 
+	private ICustomerDao customerDao;
+
+	@Autowired
+	public CustomerController(ICustomerDao customerDao) {
+		this.customerDao = customerDao;
+	}
+
 	@Override
 	protected String getViewDir() {
 		return "customer";
 	}
 
 	@RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> getCustomers(@RequestParam("offset") Integer offset, @RequestParam("limit") Integer limit) {
-		// ?offset=:offset&limit=:limit
-		// return new ResponseEntity(new ArrayList<CustomerData>());
+	public ResponseEntity<?> getCustomers(Pageable pageable) {
+		//return new ResponseEntity(customerDao.findAll(pageable));
+
 		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
 	}
 	
-	@RequestMapping(method=RequestMethod.GET, path="customers/:customerId", produces=MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method=RequestMethod.GET, path="{customerId}", produces=MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody CustomerData getCustomer(@PathParam("customerId") Long customerId) {
 		return new CustomerData();
 	}
