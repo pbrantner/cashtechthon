0088a097f9e2e7ada0451e22d27beb5ac200c1e0
Patrick Brantner <p.brantner@gmx.at>
2016-04-23 10:35:13 +0200

    Project setup

---
+++ cashtechthon/PLAYfulSavingAPI/src/main/java/at/ac/tuwien/PLAYfulSavingApiApplication.java
@@ -0,0 +1,22 @@
+package at.ac.tuwien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class PLAYfulSavingApiApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PLAYfulSavingApiApplication.class, args);
+	}
+}
