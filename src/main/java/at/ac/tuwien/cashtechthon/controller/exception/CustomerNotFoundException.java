5c8b258ed6ff271bfdc8b15fce6873972af8df48
Alwaysgone <ffxiizocker@hotmail.com>
2016-04-24 09:56:37 +0200

    changed HTTP response code from 409 to 404 if customer entity cannot be fetched by id

--- src/main/java/at/ac/tuwien/cashtechthon/controller/exception/CustomerNotFoundException.java
+++ src/main/java/at/ac/tuwien/cashtechthon/controller/exception/CustomerNotFoundException.java
@@ -3,7 +3,7 @@ package at.ac.tuwien.cashtechthon.controller.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.CONFLICT, reason="No such customer entity")
+@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such customer entity")
 public class CustomerNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
