e26805d3c415da28b1eaab259c698e744952db9f
syrenio <syrenio@gmail.com>
2016-05-21 17:13:54 +0200

    add report-controller with classification service

--- src/main/java/at/ac/tuwien/cashtechthon/controller/ReportController.java
+++ src/main/java/at/ac/tuwien/cashtechthon/controller/ReportController.java
@@ -2,15 +2,19 @@ package at.ac.tuwien.cashtechthon.controller;
 
 import at.ac.tuwien.cashtechthon.dao.ITransactionDao;
 import at.ac.tuwien.cashtechthon.domain.Transaction;
+import at.ac.tuwien.cashtechthon.dtos.Classification;
 import at.ac.tuwien.cashtechthon.dtos.CustomerReport;
+import at.ac.tuwien.cashtechthon.service.IClassificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by syrenio on 21/05/16.
@@ -20,30 +24,37 @@ import java.util.List;
 public class ReportController {
 
     private ITransactionDao transactionDao;
+    private IClassificationService classificationService;
 
     @Autowired
-    public ReportController(ITransactionDao transactionDao) {
+    public ReportController(ITransactionDao transactionDao, IClassificationService classificationService) {
         this.transactionDao = transactionDao;
+        this.classificationService = classificationService;
     }
 
     @RequestMapping(value = "/customers/{customerId}", method = RequestMethod.GET)
     public CustomerReport getTransactions(@PathVariable("customerId") Long customerId,
                                           @RequestParam(value = "age", required = false) Long age,
                                           @RequestParam(value = "income", required = false) BigDecimal income){
-        //LocalDateTime from = LocalDateTime.now().withDayOfYear(1);
-        //LocalDateTime now = LocalDateTime.now();
-        //List<Transaction> list = transactionDao.findByCustomerIdAndTransactionDateBetween(customerId, from, now);
+        LocalDate firstDayOfYear = LocalDate.now().withDayOfYear(1);
+        LocalDate now = LocalDate.now();
 
-        //return new PageImpl<Transaction>(list);
+        List<Classification> classifications = classificationService.getClassifications(new Long[]{customerId}, firstDayOfYear, now);
+
         CustomerReport report = new CustomerReport();
 
         report.getHeaders().add("Class");
         report.getHeaders().add("Money");
 
-        Object[] d = new Object[2];
-        d[0] = "Work";
-        d[1] = 12;
-        report.getData().add(d);
+        List<String> classList = classifications.stream().map(x -> x.getClassifications()).flatMap(l -> l.stream()).collect(Collectors.toList());
+
+        classList.forEach(c -> {
+            Object[] d = new Object[2];
+            d[0] = c;
+            d[1] = 999;
+            report.getData().add(d);
+        });
+
 
         return report;
     }
