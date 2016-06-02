8895de775abb9920052c2e098fcb6ae7aee32909
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2016-06-02 16:48:14 +0200

    update rest endpoint, add security tests

--- src/main/java/at/ac/tuwien/cashtechthon/config/security/JwtFilter.java
+++ src/main/java/at/ac/tuwien/cashtechthon/config/security/JwtFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -22,10 +23,12 @@ public class JwtFilter extends GenericFilterBean {
                          final ServletResponse res,
                          final FilterChain chain) throws IOException, ServletException {
         final HttpServletRequest request = (HttpServletRequest) req;
+        final HttpServletResponse response = (HttpServletResponse) res;
 
         final String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            throw new ServletException("Missing or invalid Authorization header.");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing or invalid Authorization header.");
+            return;
         }
 
         final String token = authHeader.substring(7); // The part after "Bearer "
@@ -36,7 +39,7 @@ public class JwtFilter extends GenericFilterBean {
             request.setAttribute("claims", claims);
         }
         catch (final SignatureException e) {
-            throw new ServletException("Invalid token.");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token.");
         }
 
         chain.doFilter(req, res);
--- src/main/java/at/ac/tuwien/cashtechthon/controller/ApiController.java
+++ src/main/java/at/ac/tuwien/cashtechthon/controller/ApiController.java
@@ -1,8 +1,6 @@
 package at.ac.tuwien.cashtechthon.controller;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +13,6 @@ import java.util.*;
 @RequestMapping("/api")
 public class ApiController {
 
-    private static final String SECRET_KEY = "secretkey";
-
-    private final Map<String, List<String>> userDb = new HashMap<>();
-
-    public ApiController() {
-        userDb.put("thomas", Arrays.asList("user", "admin"));
-        userDb.put("patrick", Arrays.asList("user", "admin"));
-    }
-
     @RequestMapping(value = "/role/{role}", method = RequestMethod.GET)
     public Boolean login(@PathVariable final String role,
                          final HttpServletRequest request) throws ServletException {
@@ -32,32 +21,9 @@ public class ApiController {
         return ((List<String>) claims.get("roles")).contains(role);
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public LoginResponse login(@RequestBody final UserLogin login) throws ServletException {
-        if (login.username == null || !userDb.containsKey(login.username)) {
-            throw new ServletException("Invalid login");
-        }
-
-        return new LoginResponse(Jwts.builder().setSubject(login.username)
-                .claim("roles", userDb.get(login.username)).setIssuedAt(new Date())
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact());
-    }
-
     @RequestMapping(value = "/classification", method = RequestMethod.POST)
-    public ResponseEntity<?> sendClassifications() {
+    public ResponseEntity<?> processClassification(String classification) {
+        System.out.println("new classification: " + classification);
         return new ResponseEntity<>("Classifications created", HttpStatus.CREATED);
-    }
-
-    private static class UserLogin {
-        public String username;
-        public String password;
-    }
-
-    private static class LoginResponse {
-        public String token;
-
-        public LoginResponse(final String token) {
-            this.token = token;
-        }
     }
 }
