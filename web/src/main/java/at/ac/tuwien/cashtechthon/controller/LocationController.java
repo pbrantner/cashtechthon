2cb7ee6f67db9101bbd5a8b209192878bbe2fa0e
1225449 Johannes <e1225449@student.tuwien.ac.at>
2016-06-02 18:35:56 +0200

    Added example data for /locations

--- web/src/main/java/at/ac/tuwien/cashtechthon/controller/LocationController.java
+++ web/src/main/java/at/ac/tuwien/cashtechthon/controller/LocationController.java
@@ -5,12 +5,9 @@ import at.ac.tuwien.cashtechthon.dao.ICustomerDao;
 import at.ac.tuwien.cashtechthon.dao.ITransactionDao;
 import at.ac.tuwien.cashtechthon.domain.Customer;
 import at.ac.tuwien.cashtechthon.domain.Transaction;
-import at.ac.tuwien.cashtechthon.dtos.NamedList;
+import at.ac.tuwien.cashtechthon.dtos.Locations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/locations")
 public class LocationController extends AbstractRestController {
 
-	@Autowired
-	public LocationController() { }
-
 	@RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public NamedList<String> getLocations() {
-		return null;
+	public Locations getLocations() {
+		return (new Locations()).add("Vienna").add("Graz").add("St. Pölten");
 	}
 }
