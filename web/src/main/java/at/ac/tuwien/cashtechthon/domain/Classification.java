efbbfa76b99ccc70643065dfa735a7c8993411de
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2016-06-21 11:12:42 +0200

    add classification

--- web/src/main/java/at/ac/tuwien/cashtechthon/domain/Classification.java
+++ web/src/main/java/at/ac/tuwien/cashtechthon/domain/Classification.java
@@ -1,12 +1,53 @@
 package at.ac.tuwien.cashtechthon.domain;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 public class Classification {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private Currency currency;
     private BigDecimal amount;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime classificationDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getClassificationDate() {
+        return classificationDate;
+    }
+
+    public void setClassificationDate(LocalDateTime classificationDate) {
+        this.classificationDate = classificationDate;
+    }
 }
