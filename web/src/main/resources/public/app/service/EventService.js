89c405bdb8722b72036e7604ee84f9196108629c
syrenio <syrenio@gmail.com>
2016-06-21 17:15:30 +0200

    first draft of event-service in UI

---
+++ web/src/main/resources/public/app/service/EventService.js
@@ -0,0 +1,71 @@
+(function(){
+    'use strict';
+
+    angular.module('MDC')
+        .service('EventService', ['$q','$http', '$timeout', '$mdToast', EventService]);
+
+    function EventService($q, $http, $timeout, $mdToast){
+        var self = this;
+
+        var timeoutPromise = null;
+        var userId = null;
+        var currentToast = null;
+
+        var success = function (resp) {
+            if(angular.isArray(resp.data) && resp.data.length > 0) {
+                var val = resp.data[0];
+
+                var simple = $mdToast.simple().textContent(val.text).action("OK").highlightAction(true).position("top right").hideDelay(false);
+                if(currentToast == null){
+                    currentToast = $mdToast.show(simple).then(function (answer) {
+                        if (answer === "ok") {
+                            delNotification(val.id);
+                            currentToast = null;
+                        }
+                    });
+                }
+            }
+        };
+
+        var delNotification = function (id) {
+            return $http.delete("/customers/events/" + id ).then(getNotifications);
+        };
+
+        var getNotifications = function () {
+            var query = {userId:userId};
+            return $http.get("/customers/" + userId + "/events/",{
+                params : query
+            }).then(success);
+        };
+
+        var clearNotifications = function () {
+            $mdToast.hide();
+        };
+
+        return {
+            clearNotifications: clearNotifications,
+            getNotifications : getNotifications,
+            connectUserId : function (id) {
+                // TODO start poll cycle
+                clearNotifications();
+                currentToast = null;
+
+                userId = id;
+                console.log("connect notification for user:", userId);
+                $timeout.cancel(timeoutPromise);
+
+                var poll = function () {
+                    timeoutPromise = $timeout(function () {
+                        getNotifications().then(function () {
+                            poll();
+                        });
+                    },5000);
+                };
+                poll();
+
+                getNotifications();
+            }
+        };
+    }
+
+})();
