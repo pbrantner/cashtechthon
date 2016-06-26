9be806be9aa845aa3b9a5ab931efb5cd025f7573
1225449 Johannes <e1225449@student.tuwien.ac.at>
2016-06-26 16:09:22 +0200

    Added more informative text for event toasts and restriced displaying of a reached thredhold to 1

--- web/src/main/resources/public/app/service/EventService.js
+++ web/src/main/resources/public/app/service/EventService.js
@@ -10,19 +10,68 @@
         var timeoutPromise = null;
         var userId = null;
         var currentToast = null;
+        var displayedEvents = [];
 
         var success = function (resp) {
             if(angular.isArray(resp.data) && resp.data.length > 0) {
-                var val = resp.data[0];
-                var text = "Threshold " + val.threshold.threshold + " for " + val.threshold.classification + " reached!";
-                var simple = $mdToast.simple().textContent(text).action("OK").highlightAction(true).position("top right").hideDelay(false);
-                if(currentToast == null){
-                    currentToast = $mdToast.show(simple).then(function (answer) {
-                        if (answer === "ok") {
-                            delNotification(val.id);
-                            currentToast = null;
-                        }
-                    });
+                console.log(resp.data);
+                console.log(displayedEvents);
+
+                var val = undefined;
+                var i = 0;
+                //Find first event that wasn't displayed already
+                while(val == undefined && i < resp.data.length) {
+                    if(!displayedEvents.includes(resp.data[i].threshold.id)) {
+                        val = resp.data[i];
+                    }
+                    i++;
+                }
+
+                if(val != undefined) {
+                    displayedEvents.push(val.threshold.id);
+                    
+                    var text = "";
+                    var pronome = (val.customer.gender == "Male" ? "his" : "her");
+                    if(val.threshold > 0) {
+                        text =   val.customer.firstName + " "
+                            + val.customer.lastName
+                            + " reached "
+                            + pronome
+                            + " saving goal of € "
+                            + val.threshold.threshold
+                            ;
+                    } else {
+                        text =   val.customer.firstName + " "
+                            + val.customer.lastName
+                            + " spent "
+                            + pronome
+                            + " spending limit of € "
+                            + Math.abs(val.threshold.threshold)
+                            ;
+                    }
+
+                    if(val.threshold.classification != "") {
+                        text += " in category " + val.threshold.classification;
+                    }
+
+                    if(val.threshold.windowSize ) {
+                        text += " for the last "
+                            +  (val.threshold.windowSize / (1000*60*60*24))
+                            +  " days"
+                            ;
+                    }
+                    text += "!";
+                    console.log(val);
+                    var simple = $mdToast.simple().textContent(text).action("OK").highlightAction(true).position("top right").hideDelay(false);
+
+                    if(currentToast == null){
+                        currentToast = $mdToast.show(simple).then(function (answer) {
+                            if (answer === "ok") {
+                                delNotification(val.id);
+                                currentToast = null;
+                            }
+                        });
+                    }
                 }
             }
         };
